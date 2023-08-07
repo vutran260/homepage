05a01f3b732bcff497d8050183a3ee67b9141856
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-08-07 22:13:43 +0700

    update package.json

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import { animated, useSpring } from 'react-spring'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Typist from 'react-typist'
 import { AppContext } from './contexts/app.context'
 import Contact from './pages/Contact'
 import HomePage from './pages/HomePage'
@@ -12,20 +11,10 @@ import PostDetail from './pages/PostDetail'
 import PostsList from './pages/PostsList/Postlists'
 import http from './utils/http'
 
-const AnimationWrap = ({ children }) =>
-  // <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
-  ({ children })
-  // </motion.div>
-
 function App() {
   const { setSetting } = useContext(AppContext)
   const [loading, setLoading] = useState(true)
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //   }, 3000)
-  // }, [])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,22 +24,8 @@ function App() {
     fetchData()
   }, [])
 
-  const fadeAndScaleIn = useSpring({
-    opacity: loading ? 0 : 1,
-    // transform: loading ? 'scale(0.8)' : 'scale(1)'
-  })
-
   return (
     <div className='App'>
-      {/* {loading ? (
-        <div className='loading-spinner justify-center text-center text-black'>
-          <Typist avgTypingDelay={100} startDelay={0} className='text-4xl italic'>
-            WELCOME...
-          </Typist>
-        </div>
-      ) :  */}
-      (
-        {/* <animated.h1 style={fadeAndScaleIn}> */}
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/posts' element={<PostsList />} />
@@ -58,9 +33,6 @@ function App() {
             <Route path='/contact' element={<Contact />} />
             <Route path='/portfolio' element={<Portfolio />} />
           </Routes>
-        {/* </animated.h1> */}
-      )
-
       <ToastContainer />
     </div>
   )
