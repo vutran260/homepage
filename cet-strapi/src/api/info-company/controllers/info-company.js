cab93d650cbd3444948829003193819d5ac89fbe
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-09-16 07:11:36 +0000

    add api info-company

---
+++ cet-strapi/src/api/info-company/controllers/info-company.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * info-company controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::info-company.info-company');
