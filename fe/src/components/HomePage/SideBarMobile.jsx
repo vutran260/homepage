224c47fe5b68eec2ceffa61b89f482544928ce3a
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-09-25 21:58:35 +0700

    hidden info-company sidebar-mobile

--- fe/src/components/HomePage/SideBarMobile.jsx
+++ fe/src/components/HomePage/SideBarMobile.jsx
@@ -16,7 +16,7 @@ export const SideBarMobile = ({position, handleCloseSideBar}) => {
     'Value',
     'Team',
     'Feedback',
-    'CompanyInfo',
+    // 'CompanyInfo',
     'Contact_Us',
   ]
   const arrItemSideBar = [
@@ -28,7 +28,7 @@ export const SideBarMobile = ({position, handleCloseSideBar}) => {
     '価値提供',
     'チームについて',
     'フィードバック',
-    '会社概要',
+    // '会社概要',
     'お問い合わせ'
   ]
 
