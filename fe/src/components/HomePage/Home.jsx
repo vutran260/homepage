0afb20c2f04900f6fe019da781d7fad33ec8d3eb
Vu Tran <vutran@192.168.0.105>
2025-03-09 00:48:44 +0700

    feat/revert-home-background

--- fe/src/components/HomePage/Home.jsx
+++ fe/src/components/HomePage/Home.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { ButtonGradient } from 'src/components/Button'
 import { AnimationFadeInUp } from 'src/components/Animation/index.jsx'
+import bgHomeVideo from 'src/assets/videos/bgHome.mp4'
+import bgHomeMobile from 'src/assets/images/bgHomeMobile.png'
 
-export function Home({ shouldAnimate }) {
+export function Home({ shouldAnimate, isMobile }) {
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    if (shouldAnimate && videoRef.current) {
+      const isIOSSafari =
+        /iPhone|iPad|iPod/i.test(navigator.userAgent) && /WebKit/i.test(navigator.userAgent) && !window.MSStream
+
+      if (!isIOSSafari) {
+        const playPromise = videoRef.current.play()
+
+        if (playPromise !== undefined) {
+          playPromise.catch((error) => {
+            console.log('Autoplay prevented:', error)
+          })
+        }
+      }
+    }
+  }, [shouldAnimate])
+
   return (
-    <div className='home-container'>
-      <AnimationFadeInUp
-        shouldAnimate={shouldAnimate}
-        className='pb-6 text-center text-4xl leading-tight tracking-widest xl:text-6xl'
-        index={0}
-      >
-        アプリ開発といえば
-      </AnimationFadeInUp>
-      <div className='bg-home h-screen pt-4'>
+    <div
+      className='home-container bg-cover bg-center bg-no-repeat md:bg-none'
+      style={{ backgroundImage: `url(${bgHomeMobile})` }}
+    >
+      <div className='z-10 h-screen pt-4'>
         <div className='flex flex-row justify-center'>
           <div className='textHome mb-10 flex flex-col items-center text-white'>
+            <AnimationFadeInUp
+              shouldAnimate={shouldAnimate}
+              className='pb-6 text-center text-4xl leading-tight tracking-widest xl:text-6xl'
+              index={0}
+            >
+              アプリ開発といえば
+            </AnimationFadeInUp>
             <AnimationFadeInUp
               shouldAnimate={shouldAnimate}
               className='text-center font-secondBold text-9xl font-bold leading-tight tracking-widest md:text-9xl'
@@ -30,6 +54,18 @@ export function Home({ shouldAnimate }) {
           </div>
         </div>
       </div>
+      {!isMobile && (
+        <video
+          autoPlay={false}
+          ref={videoRef}
+          loop
+          muted
+          className='video-background absolute inset-0 h-full w-full object-cover'
+        >
+          <source src={bgHomeVideo} type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+      )}
     </div>
   )
 }
